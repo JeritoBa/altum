1e26b65070297f692b759348623c8a88d30de1eb
JeritoBa <jerogallego099@gmail.com>
2026-06-23 19:28:47 -0500

    fix: Inside database, added 'Accepted' and 'Rejected' states

---
+++ Migrations/20260624002812_BookingStateUpdated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace main.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookingStateUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
